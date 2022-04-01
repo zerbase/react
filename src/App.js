a61ffd15640b6dae8cca7f8cfd3584bbad1b76b8
zerbase <zerbase@naver.com>
2022-04-02 08:18:19 +0900

    react day 2

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '강남 고기 맛집';
+  let cs  = {
+    color: 'red',
+    fontSize:'30px'
+  }
+  function 함수() {
+    return 100
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div style={cs}>개발 Blog</div>
+      </div>    
+      <img src={logo} />
+      <h4>{함수()}</h4>
     </div>
   );
 }
