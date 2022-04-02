3ce778ae04dd8522b4835d1d3fb63dabfe168a39
zerbase <zerbase@naver.com>
2022-04-03 08:31:53 +0900

    react day 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+  // var [c,d] = [10, 100];
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','양재동 부동산']);
+  
   let posts = '강남 고기 맛집';
   let cs  = {
-    color: 'red',
-    fontSize:'30px'
+    color: 'white',
+    fontSize:'20px'
   }
   function 함수() {
     return 100
@@ -16,7 +19,22 @@ function App() {
       <div className='black-nav'>
         <div style={cs}>개발 Blog</div>
       </div>    
-      <img src={logo} />
+      <div className='list'>
+        <h3> { 글제목[0]} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> { 글제목[1]} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> { 글제목[2]} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      {/* <img src={logo} /> */}
       <h4>{함수()}</h4>
     </div>
   );
